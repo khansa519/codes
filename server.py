fa70d766fe3806e97bdafd331eac45e30d348980
khansa519 <salaljamil999@gmail.com>
2016-05-19 19:06:13 +0500

    Update server.py

--- server.py
+++ server.py
@@ -22,18 +22,37 @@ print ('Socket bind complete')
 s.listen(2)
 print ('Socket now listening')
  
-
+conn={}
+addr={}
 while 1:
-    conn, addr = s.accept()
-    print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+    conn[0], addr[0] = s.accept()
+    conn[1], addr[1] = s.accept()
+   # print ('Connected with ' + addr[0] + ':' + str(addr[1]))
 while True:
-    data = conn.recv(1024)
-    print ('received "/s" ' % data)
+    
+    data = conn[0].recv(1024)
     if not data:
         print ('there is no more message')
         break
     else:
         print ('sending data to other ')
-        conn.sendall(data)
-conn.close()    
+        reply= +data
+        print (reply)
+        
+        conn[0].sendall(reply)
+
+    data1 = conn[1].recv(1024)
+    if not data1:
+        print ('there is no more message')
+        break
+    else:
+        print ('sending data to other ')
+        reply1= +data1
+        print (reply1)
+        
+        conn[1].sendall(reply1)
+        
+conn[0].close()
+conn[1].close()
 s.close()
+
