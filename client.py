8abb8e1549cc7d1e8a9f5ae684ac1605b9e52ea4
khansa519 <salaljamil999@gmail.com>
2016-05-20 19:32:17 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,26 @@
 
 import socket
 import sys
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+from thread import *
+
+s = socket.socket()
 HOST = socket.gethostname()
-PORT = 5812                # Reserve a port for your service.
+PORT = 5812  # Reserve a port for your service.
+
+
+def cthread():
+    while True:
+        received = s.recv(1024)
+        print 'received: ', received
 
 s.connect((HOST, PORT))
+start_new_thread(cthread, ())
+
 while True:
-    s.sendall(raw_input(data))
+    msg = raw_input()
+    s.sendall(msg)
     print ('message is being sending')
 
-    received = str(s.recv(1024))  
-    print ('received : ' , received)
-s.close                     # Close the socket when done
+
+s.close  # Close the socket when done
+
