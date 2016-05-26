2822455f316c5d357c97592c40473e27d1b82378
khansa519 <salaljamil999@gmail.com>
2016-05-27 00:12:59 +0500

    Add files via upload

---
+++ client_module2.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+from thread import *
+
+s = socket.socket()
+HOST = socket.gethostname()
+PORT = 5812  # Reserve a port for your service.
+
+
+def cthread():
+    while True:
+        received = s.recv(1024)
+        print 'received: ', received
+
+s.connect((HOST, PORT))
+start_new_thread(cthread, ())
+
+while True:
+    msg = raw_input()
+    s.sendall(msg)
+    print ('message sent\n')
+
+
+s.close  # Close the socket when done
