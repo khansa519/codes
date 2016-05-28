74c0cf04aa0a799a5880ec90c5bd89c7397f8178
khansa519 <salaljamil999@gmail.com>
2016-05-28 16:39:35 +0500

    Update client_module2.py

--- client_module2.py
+++ client_module2.py
@@ -10,7 +10,7 @@ PORT = 5812  # Reserve a port for your service.
 def cthread():
     while True:
         received = s.recv(1024)
-        print 'received: ', received
+        print  received
 
 s.connect((HOST, PORT))
 start_new_thread(cthread, ())
@@ -18,7 +18,7 @@ start_new_thread(cthread, ())
 while True:
     msg = raw_input()
     s.sendall(msg)
-    print ('message sent\n')
+   # print ('message sent\n')
 
 
 s.close  # Close the socket when done
